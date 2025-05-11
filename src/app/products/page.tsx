34f4ae3821a8d417e8ad1dcceea8369fbf251e5f
HubertDudaDeveloper <102989117+HubertDudaDeveloper@users.noreply.github.com>
2025-05-11 16:30:33 +0200

    update

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,56 @@
-export const ProductsList = () => {
+import { EDummyJSON, IProduct, IProductQuery } from "@/types/ProductTypes";
+import Image from "next/image";
+
+export const ProductsList = async () => {
+  let products: IProduct[] = [];
+
+  try {
+    const loadProducts: Promise<IProductQuery> = (await (
+      await fetch(`${EDummyJSON.URL}${EDummyJSON.PRODUCTS}`)
+    ).json()) as unknown as Promise<IProductQuery>;
+    products = (await loadProducts).products as IProduct[];
+  } catch (e) {
+    console.log(e);
+  } finally {
+  }
+
   return (
-    <>
-    test stronki
-    </>
-  )
-}
+    <div
+      className="
+        flex flex-col gap-[24]
+        justify-center-safe items-center
+        p-[24]  w-[100%]
+      "
+    >
+      <h1 className="text-4xl font-bold">Products list</h1>
+      <div
+        className="
+          flex flex-row flex-wrap gap-[24]
+          justify-center-safe items-stretch
+          w-[100%] max-w-[1366]
+        "
+      >
+        {products.map((product: IProduct) => {
+          return (
+            <div
+              className="
+                flex flex-col gap-[16]
+                justify-center-safe items-center
+                h-auto w-100 p-[16]
+                border border-indigo-600 rounded-md
+              "
+              key={product.id}
+            >
+              <Image src={product.images[0]} width={250} height={250} alt="" />
+              <h3 className="font-bold">{product.title}</h3>
+              <p className="line-clamp-2">{product.description}</p>
+              <span className="font-bold">{product.price}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default ProductsList;
